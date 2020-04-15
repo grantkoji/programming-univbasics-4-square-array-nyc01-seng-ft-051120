f96a91eb128cdbe0d518077e41c25a4c80d4c919
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-15 19:56:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,12 +1,10 @@
 
 def square_array(array)
   # your code here
-  index = 0
   newarray = []
   length = array.length
-  while index < length do
+  length.times do |index|
     newarray.push(array[index] ** 2)
-    index += 1
   end
   return newarray
 end
