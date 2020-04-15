cb0f594536e5239c9ef8b4517d7a15adfcc9a89e
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-15 19:43:02 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,10 @@
+require 'pry'
+
 def square_array(array)
   # your code here
   newarray = []
   length = array.length
+  binding.pry
   length.times { |index|
     newarray[index] = (array[index])**
   }
